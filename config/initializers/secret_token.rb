f79d49e8308ed11ec679cfdc29efc5c67c7bba4e
Gant <GantMan@gmail.com>
2014-05-05 19:04:00 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Naughtyfish::Application.config.secret_key_base = '99128fa23b48efc76792aefe487d7f53551361abf99ba7c01d78fb0728ff5617f88e59830f75684a959ab494732fd6dab5cf1b37a1a9de2ff432210ac4011ade'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Naughtyfish::Application.config.session_store :cookie_store, key: '_naughtyfish_session'
